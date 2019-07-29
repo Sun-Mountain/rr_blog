b119ca3b7059334e8947c1240b79cc56d3966747
Sun-Mountain <npzonnenberg@gmail.com>
2019-07-29 15:36:26 -0400

    comments destroy

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -7,6 +7,10 @@ class CommentsController < ApplicationController
     end
 
     def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        redirect_to post_path(@post)
     end
 
 end
